99dbe71e14b80c487af3a50f78016330b37e9b24
Slater Bernstein <sbernstein@dev-10.com>
2023-04-10 19:18:44 -0500

    4/10/2023 EOD commit

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
 // Smooth scrolling for internal links
-var navLinks = document.querySelectorAll('nav ul li a');
+var navLinks = document.querySelectorAll("nav ul li a");
 
 for (var i = 0; i < navLinks.length; i++) {
-	navLinks[i].addEventListener('click', function(event) {
-		event.preventDefault();
-		var targetSection = document.querySelector(this.hash);
-		smoothScroll(targetSection);
-	});
+  navLinks[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    var targetSection = document.querySelector(this.hash);
+    smoothScroll(targetSection);
+  });
 }
 
 function smoothScroll(targetSection) {
-	var targetSectionPosition = targetSection.offsetTop;
-	var currentPosition = window.pageYOffset;
-	var distance = targetSectionPosition - currentPosition;
-	var duration = 1000;
-	var startTime = null;
+  var targetSectionPosition = targetSection.offsetTop;
+  var currentPosition = window.pageYOffset;
+  var distance = targetSectionPosition - currentPosition;
+  var duration = 1000;
+  var startTime = null;
 
-	function animation(currentTime) {
-		if (startTime === null) startTime = currentTime;
-		var timeElapsed = currentTime - startTime;
-		var run = ease(timeElapsed, currentPosition, distance, duration);
-		window.scrollTo(0, run);
-		if (timeElapsed < duration) requestAnimationFrame(animation);
-	}
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, currentPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
 
-	function ease(t, b, c, d) {
-		t /= d / 2;
-		if (t < 1) return c / 2 * t * t + b;
-		t--;
-		return -c / 2 * (t * (t - 2) - 1) + b;
-	}
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
 
-	requestAnimationFrame(animation);
+  requestAnimationFrame(animation);
 }
 
-const buttons = document.querySelectorAll('.social-button');
+const buttons = document.querySelectorAll(".social-button");
 
-buttons.forEach(button => {
-  button.addEventListener('mouseover', () => {
-    button.classList.add('fa-shake');
+buttons.forEach((button) => {
+  button.addEventListener("mouseover", () => {
+    button.classList.add("fa-shake");
   });
-  
-  button.addEventListener('mouseout', () => {
-    button.classList.remove('fa-shake');
+
+  button.addEventListener("mouseout", () => {
+    button.classList.remove("fa-shake");
   });
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
